b9d85ae9cf01b29c80fb498b239fea1abc512fac
hytkgami <gagagatrump@gmail.com>
2023-06-28 23:30:37 +0900

    add rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,13 @@
+import rss, {pagesGlobToRssItems} from '@astrojs/rss';
+
+export async function get() {
+  return rss(
+    {
+      title: 'Astro Learner | Blog',
+      description: 'My journey learning Astro',
+      site: 'https://astro-tutorial-psi.vercel.app/',
+      items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+      customData: `<language>en-us</language>`,
+    }
+  )
+}
